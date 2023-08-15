4325dbda8374c6abb0e38861a90023285ff0f039
ANekhai <nekhai.anton@gmail.com>
2023-08-14 21:51:30 -0400

    WIP

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import TensorType
 from .model import VAE
 
 import os
@@ -9,6 +10,7 @@ class TrainParams:
     epochs: int = 200
     batch_size: int = 128
     lr: float = 0.0003
+    device: str = ...
 
     data_root: str = "./data"
     csv_path: str = "./pokemon.csv"
@@ -19,6 +21,21 @@ class TrainParams:
     to_checkpoint: bool = True
     cp_out_path: str =  ""
     
+    save_animation: bool = True
+    anim_per_epoch: int = 20
+
+
+class AnimationHandler():
+    def __init__(self, ):
+        self.animation_frames: list[TensorType] = []
+        self.random_batch
+
+    def push_frame(self):
+        pass
+
+    def save_animation(self, path):
+        pass
+
 
 
 def ELBOLoss():
