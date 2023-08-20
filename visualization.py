1260f70fadb14bacaf2cab461468bbaf7c7bfa78
Anton <nekhai.anton@gmail.com>
2023-08-20 01:57:10 -0400

    implemented slerp

--- visualization.py
+++ visualization.py
@@ -1,14 +1,30 @@
 import torch
+from torch import Tensor
 import torchvision.utils as vutils
 
+from numpy import arccos, sin
 
 class AnimationHandler():
-    def __init__(self, batch:torch.dtype=None):
-        self.animation_frames: list[torch.dtype] = []
-        self.batch: torch.dtype = batch
+    def __init__(self, batch:Tensor=None):
+        self.animation_frames = []
+        self.batch = batch
 
-    def push_frame(self, new_frame:torch.dtype):
+    def push_frame(self, new_frame: Tensor):
         pass
 
     def save_animation(self, path:str, show=True):
         pass
+
+def slerp(v1: Tensor, v2: Tensor, steps:int):
+    ''' Spherical linear interpolation between two vectors '''
+    omega = arccos( (v1 @ v2).item() )
+    interpolation = lambda v, w, t: sin((1-t)*omega)/sin(omega) * v + sin(t*omega)/sin(omega) * w
+    steps = [i * (1/steps) for i in range(steps+1)]
+    return [interpolation(v1, v2, step) for step in steps] 
+
+
+if __name__ == "__main__":
+    v1 = torch.tensor([1., 0., 0.])
+    v2 = torch.tensor([0., 0.5, 1.])
+
+    print(slerp(v1, v2, 3))
